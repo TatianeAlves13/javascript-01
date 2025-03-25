34465c2e030549fce50b34e33916cd4e048c0f99
TatianeAlves13 <alves1395.thatyanne@gmail.com>
2025-03-24 21:16:01 -0300

    conclução

---
+++ app.js
@@ -0,0 +1,12 @@
+//alert("olá, seja bem vindo")//
+console.log("posso escrever aqui também")
+console.log(10+2)
+console.log(10-2)
+console.log(10*2)
+console.log(10/5)
+alert(10+2)
+console.log("10" + 2)
+console.log("10"*2)
+console.log("João" + " " + "Paulo")//concatenação , se tem o texto e o num ele vai forçar virar um numero//
+//num+num= ele vai adicionar; texto+num= concatenação; num+txt= concatenação; txt+txt= concatenação. essa concatenação nada mais é que txt string// 
+console.log(parseFloat("10")+2)//parseFloat conversação que é um numero decimal//
